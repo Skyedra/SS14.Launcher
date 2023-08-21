efa61416b325ba8c10dd56cb5529a61484400554
sTiKyt <35602029+sTiKyt@users.noreply.github.com>
2023-08-21 18:55:30 +0300

    Added a script that would automatically replace Launcher on Steam and allow using Multiverse as a drop-in replacement

---
+++ SteamMod/LauncherLinux.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Check if ".data_moved" file exists
+if [ ! -f .data_moved ]; then
+    # Check if ".local/share/Space Station 14/launcher" exists
+    if [ -d "$HOME/.local/share/Space Station 14/launcher" ]; then
+        echo "Renaming 'launcher' folder to 'launcher-skyedra'..."
+        cp "$HOME/.local/share/Space Station 14/launcher" "$HOME/.local/share/Space Station 14/launcher-skyedra"
+        echo "Folder renaming complete."
+
+        echo "Creating '.data_moved' file..."
+        touch .data_moved
+    else
+        echo "'launcher' folder not found. Skipping renaming."
+    fi
+else
+    echo "'.data_moved' file exists. Skipping folder renaming and file creation."
+fi
+
+# Check if ".multiverse" file exists
+if [ -f .multiverse ]; then
+    echo "'.multiverse' file found. Skipping script and launching SS14.Launcher."
+    ./SS14.Launcher
+else
+    echo "Downloading SS14.Launcher_Linux.zip..."
+    wget https://cdn.spacestationmultiverse.com/launcher-builds/SS14.Launcher_Linux.zip
+
+    echo "Deleting existing files except the script and zip..."
+    find . -maxdepth 1 ! -name "$(basename $0)" ! -name "SS14.Launcher_Linux.zip" -exec rm -r {} \;
+
+    echo "Extracting SS14.Launcher_Linux.zip..."
+    unzip SS14.Launcher_Linux.zip
+    rm SS14.Launcher_Linux.zip
+    touch .multiverse
+
+    echo "Script execution complete."
+fi
---
+++ SteamMod/README.md
@@ -0,0 +1,13 @@
+## How to Install the Multiverse Launcher as a Drop-In Replacement on Steam
+#### Why? This enables you to maintain your playtime while enjoying our launcher's enhanced quality and unique features, along with multihub support.
+### On Linux:
+1. Navigate to `Space Station 14 Playtest` in your Steam library.
+2. Right-click, then choose Manage > Browse local files.
+3. Copy the `LauncherLinux.sh` file into the `Space Station 14 Playtest` directory.
+4. Return to `Space Station 14 Playtest` in Steam. Right-click, then select Properties...
+5. In the `LAUNCH OPTIONS` section, add `./LauncherLinux.sh %command%`.
+6. Click the `PLAY` button, wait a minute or two, and enjoy your game!
+
+### On Windows
+
+# TODO
