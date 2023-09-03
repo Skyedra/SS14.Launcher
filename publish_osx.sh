c4132c129c7a048c0c57951d649fcf479f8ace3e
Skye <22365940+Skyedra@users.noreply.github.com>
2023-09-02 17:59:51 -0700

    SSMV Output binary renaming: OSX (untested)

--- publish_osx.sh
+++ publish_osx.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 
 # Clear out previous build.
 rm -r **/bin bin/publish/macOS
-rm SS14.Launcher_macOS.zip
+rm SSMV.Launcher_macOS.zip
 
 dotnet publish SS14.Launcher/SS14.Launcher.csproj /p:FullRelease=True -c Release --no-self-contained -r osx-x64 /nologo /p:RobustILLink=true
 dotnet publish SS14.Loader/SS14.Loader.csproj -c Release --no-self-contained -r osx-x64 /nologo
@@ -14,16 +14,16 @@ dotnet publish SS14.Loader/SS14.Loader.csproj -c Release --no-self-contained -r 
 # Create intermediate directories.
 mkdir -p bin/publish/macOS
 
-cp -r "PublishFiles/Space Station 14 Launcher.app" bin/publish/macOS
+cp -r "PublishFiles/Space Station Multiverse Launcher.app" bin/publish/macOS
 
-mkdir -p "bin/publish/macOS/Space Station 14 Launcher.app/Contents/Resources/dotnet/"
-mkdir -p "bin/publish/macOS/Space Station 14 Launcher.app/Contents/Resources/bin/"
-mkdir -p "bin/publish/macOS/Space Station 14 Launcher.app/Contents/Resources/bin/loader/Space Station 14.app/Contents/Resources/bin/"
+mkdir -p "bin/publish/macOS/Space Station Multiverse Launcher.app/Contents/Resources/dotnet/"
+mkdir -p "bin/publish/macOS/Space Station Multiverse Launcher.app/Contents/Resources/bin/"
+mkdir -p "bin/publish/macOS/Space Station Multiverse Launcher.app/Contents/Resources/bin/loader/Space Station 14.app/Contents/Resources/bin/"
 
-cp -r Dependencies/dotnet/mac/* "bin/publish/macOS/Space Station 14 Launcher.app/Contents/Resources/dotnet/"
-cp -r SS14.Launcher/bin/Release/net7.0/osx-x64/publish/* "bin/publish/macOS/Space Station 14 Launcher.app/Contents/Resources/bin/"
-cp -r SS14.Loader/bin/Release/net7.0/osx-x64/publish/* "bin/publish/macOS/Space Station 14 Launcher.app/Contents/Resources/bin/loader/Space Station 14.app/Contents/Resources/bin/"
+cp -r Dependencies/dotnet/mac/* "bin/publish/macOS/Space Station Multiverse Launcher.app/Contents/Resources/dotnet/"
+cp -r SS14.Launcher/bin/Release/net7.0/osx-x64/publish/* "bin/publish/macOS/Space Station Multiverse Launcher.app/Contents/Resources/bin/"
+cp -r SS14.Loader/bin/Release/net7.0/osx-x64/publish/* "bin/publish/macOS/Space Station Multiverse Launcher.app/Contents/Resources/bin/loader/Space Station 14.app/Contents/Resources/bin/"
 cp LICENSE.txt bin/publish/macOS/
 pushd bin/publish/macOS
-zip -r ../../../SS14.Launcher_macOS.zip *
+zip -r ../../../SSMV.Launcher_macOS.zip *
 popd
