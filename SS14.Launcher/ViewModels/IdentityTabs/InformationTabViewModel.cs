70aade0e93c41585dd30a50ef09d0a948e111727
Skye <22365940+Skyedra@users.noreply.github.com>
2023-09-29 18:00:13 -0700

    translation - workaround to be able to translate create identity paragraph

--- SS14.Launcher/ViewModels/IdentityTabs/InformationTabViewModel.cs
+++ SS14.Launcher/ViewModels/IdentityTabs/InformationTabViewModel.cs
@@ -19,4 +19,21 @@ public class InformationTabViewModel : IdentityTabViewModel
     }
 
     public override string Name => Loc.GetParticularString("Create Identity Tab", "(Information)");
+
+    public string WelcomeText
+    {
+        get
+        {
+            return Loc.GetString(@"Let's set up your identity!
+
+From this screen, you can create an identity on one of the established authentication providers, or just use a guest login.
+
+Different servers accept different ways to authenticate you, and not all servers accept all authentication providers.
+
+It is possible to manage multiple identities using this launcher, just revisit this page to establish them as needed.  (Though you probably do not want to create more than one identity per provider).
+
+Start by choosing an identity provider on the left.  You can revisit this screen later to create identities at other providers, should you wish to do so.");
+        }
+    }
+
 }
