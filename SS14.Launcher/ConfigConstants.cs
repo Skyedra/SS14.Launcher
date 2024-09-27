cb92a8e9243cb0cd5e001ef0dcd07bad814b5b1b
Skye <22365940+Skyedra@users.noreply.github.com>
2024-09-27 02:39:52 -0700

    Fix bug report regarding unable to run both 14 & MV at same time

--- SS14.Launcher/ConfigConstants.cs
+++ SS14.Launcher/ConfigConstants.cs
@@ -20,7 +20,7 @@ public static class ConfigConstants
     // Check the command queue this often.
     public static readonly TimeSpan CommandQueueCheckInterval = TimeSpan.FromSeconds(1);
 
-    public const string LauncherCommandsNamedPipeName = "SS14.Launcher.CommandPipe";
+    public const string LauncherCommandsNamedPipeName = "SSMV.Launcher.CommandPipe";
     // Amount of time to wait before the launcher decides to ignore named pipes entirely to keep the rest of the launcher functional.
     public const int LauncherCommandsNamedPipeTimeout = 150;
     // Amount of time to wait to let a redialling client properly die
