eb617bcc0620f5c21681bdb21dfd1bdd8bbfd0af
Skye <22365940+Skyedra@users.noreply.github.com>
2025-11-24 23:24:20 -0800

    Require upgrade since otherwise new version servers will be unconnectable

--- SS14.Launcher/ConfigConstants.cs
+++ SS14.Launcher/ConfigConstants.cs
@@ -5,7 +5,7 @@ namespace SS14.Launcher;
 
 public static class ConfigConstants
 {
-    public const string CurrentLauncherVersion = "3";
+    public const string CurrentLauncherVersion = "4";
     public static readonly bool DoVersionCheck = true;
 
     // Refresh login tokens if they're within <this much> of expiry.
